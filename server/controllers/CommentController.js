4ce3fdcb8439ec5411dee888bbee41769a4db11b
SeanFuller294 <s.fuller294@hotmail.com>
2019-09-16 11:53:57 -0600

    Basic Server Setup

---
+++ server/controllers/CommentController.js
@@ -0,0 +1,54 @@
+import _commentService from '../services/CommentService'
+import express from 'express'
+import { Authorize } from '../middleware/authorize.js'
+
+//PUBLIC
+export default class ListController {
+  constructor() {
+    this.router = express.Router()
+      .use(Authorize.authenticated)
+      .get('', this.getAll)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+      .use(this.defaultRoute)
+  }
+
+  defaultRoute(req, res, next) {
+    next({ status: 404, message: 'No Such Route' })
+  }
+
+  async getAll(req, res, next) {
+    try {
+      //only gets boards by user who is logged in
+      let data = await _commentService.find({ taskId: req.body.taskId })
+      return res.send(data)
+    }
+    catch (err) { next(err) }
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.authorId = req.session.uid
+      let data = await _commentService.create(req.body)
+      return res.status(201).send(data)
+    } catch (error) { next(error) }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await _commentService.findOneAndUpdate({ _id: req.params.id, authorId: req.session.uid }, req.body, { new: true })
+      if (data) {
+        return res.send(data)
+      }
+      throw new Error("invalid id")
+    } catch (error) { next(error) }
+  }
+
+  async delete(req, res, next) {
+    try {
+      await _commentService.findOneAndRemove({ _id: req.params.id, authorId: req.session.uid })
+      return res.send("Successfully deleted")
+    } catch (error) { next(error) }
+  }
+}
