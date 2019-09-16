1ae8e35af1744f1e5563924e1805ee7ee8f2ced1
SeanFuller294 <s.fuller294@hotmail.com>
2019-09-16 11:37:20 -0600

    added tasks to server

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -1,9 +1,9 @@
-import _listService from '../services/ListService'
+import _taskService from '../services/TaskService'
 import express from 'express'
 import { Authorize } from '../middleware/authorize.js'
 
 //PUBLIC
-export default class ListController {
+export default class TaskController {
   constructor() {
     this.router = express.Router()
       .use(Authorize.authenticated)
@@ -21,7 +21,7 @@ export default class ListController {
   async getAll(req, res, next) {
     try {
       //only gets boards by user who is logged in
-      let data = await _listService.find({ boardId: req.body.boardId })
+      let data = await _taskService.find({ listId: req.body.listId })
       return res.send(data)
     }
     catch (err) { next(err) }
@@ -30,14 +30,14 @@ export default class ListController {
   async create(req, res, next) {
     try {
       req.body.authorId = req.session.uid
-      let data = await _listService.create(req.body)
+      let data = await _taskService.create(req.body)
       return res.status(201).send(data)
     } catch (error) { next(error) }
   }
 
   async edit(req, res, next) {
     try {
-      let data = await _listService.findOneAndUpdate({ _id: req.params.id, authorId: req.session.uid }, req.body, { new: true })
+      let data = await _taskService.findOneAndUpdate({ _id: req.params.id, authorId: req.session.uid }, req.body, { new: true })
       if (data) {
         return res.send(data)
       }
@@ -47,7 +47,7 @@ export default class ListController {
 
   async delete(req, res, next) {
     try {
-      await _listService.findOneAndRemove({ _id: req.params.id, authorId: req.session.uid })
+      await _taskService.findOneAndRemove({ _id: req.params.id, authorId: req.session.uid })
       return res.send("Successfully deleted")
     } catch (error) { next(error) }
   }
